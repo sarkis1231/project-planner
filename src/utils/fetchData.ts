9a5c9f7d9020e1e716351728a465c286099a3eb4
sarkis1231 <sakooghly@gmail.com>
2021-12-25 16:31:24 +0400

    refactoring: fetchData

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,9 +1,11 @@
-export const fetchData = async (url: string): Promise<unknown> => {
+export const fetchData = async <T>(url: string, method = "GET"): Promise<T> => {
+  const DOMAIN = "http://localhost:3000/";
   try {
-    const res = await fetch(`http://localhost:3000/${url}`);
+    const res = await fetch(`${DOMAIN}${url}`, { method });
     const data = await res.json();
     return data;
-  } catch (err: unknown) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
     return err;
   }
 };
