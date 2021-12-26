720366c709286b780e5071e91fde302ff07f7a44
sarkis1231 <sakooghly@gmail.com>
2021-12-26 19:27:07 +0400

    feat: NavBar component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
+  <NavBar />
   <router-view />
 </template>
+
+<script lang="ts">
+import NavBar from "./components/NavBar.vue";
+
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
 
 <style>
 body {
